ba4833ebc2b4d588e115da9e6db197abbddeb657
shanewalsh1999 <shanewalsh1999@gmail.com>
2021-03-07 18:22:37 -0500

    finished clock code

--- tens_unit_clock_v2/tens_unit_clock_v2.ino
+++ tens_unit_clock_v2/tens_unit_clock_v2.ino
@@ -1,7 +1,19 @@
 #include <SoftwareSerial.h>
 SoftwareSerial EEBlue(0, 1); // RX | TX
 
+static const int lowFreqPin = 3;
+static const int medFreqPin = 6;
+static const int highFreqPin = 9;
+
+// Frequency and pulse width variables
+int encodedValue;
+int frequencyID;
+int pulseWidthID;
+
 void setup() {
+  Serial.begin(9600);
+  EEBlue.begin(9600);
+  
   //------- Set PWM frequency for D3 & D11 -------
   // set timer 2 divisor to  1024 for PWM frequency of 30.64 Hz
   TCCR2B = TCCR2B & B11111000 | B00000111;    
@@ -16,7 +28,7 @@ void setup() {
   TCCR0B = TCCR0B & B11111000 | B00000101;  
   
   // pulse width can be adjusted in steps of 64.25 microseconds
-  //  e.g. analogWrite(5, 1) = pulse width of 64.25us on pin 5
+  //  e.g. analogWrite(6, 1) = pulse width of 64.25us on pin 5
   // --------------------------------------------
   
   //------- Set PWM frequency for D9 & D10 -------
@@ -24,21 +36,49 @@ void setup() {
   TCCR1B = TCCR1B & B11111000 | B00000100;
 
   // pulse width can be adjusted in steps of 32.13 microseconds
-  //  e.g. analogWrite(9, 2) = pulse width of 32.13*2us on pin 9
+  //  e.g. analogWrite(9, 3) = pulse width of 32.13*3us on pin 9
   //----------------------------------------------
-
-  // Pin 5: 61.04 Hz, 64.25*1 = 64.245us pulse width
-  // analogWrite(5, 1);
-  
-  // Pin 9: 30.64 Hz, 127.49*3 = 382.47us pulse width
-  // analogWrite(9, 3);
-  
 }
 
 void loop() { 
-  // Display data sent over bluetooth
   if (EEBlue.available()) {
-    int encodedVal = EEBlue.read();
-    Serial.write(encodedValue);
+    // encodedValue:
+    //  - frequencyID in tens place
+    //  - pulseWidthID in ones place
+    encodedValue = EEBlue.read();
+
+    // Parse encoded value for frequency and pulse width IDs
+    frequencyID = encodedValue / 10;
+    pulseWidthID = encodedValue % 10;
+
+    // Reset all output pins to avoid concurrent output signals
+    analogWrite(lowFreqPin, 0);
+    analogWrite(medFreqPin, 0);
+    analogWrite(highFreqPin, 0);
+    
+    // Output clock signal
+    switch(frequencyID) {
+      // 30.64 Hz 
+      case 1:
+        analogWrite(lowFreqPin, pulseWidthID);
+        break;
+      
+      // 61.04 Hz
+      case 2:
+        analogWrite(medFreqPin, pulseWidthID);
+        break;
+      
+      // 122.07 Hz
+      case 3:
+        analogWrite(highFreqPin, pulseWidthID);
+        break;
+    }
+    
+    // Display data on serial monitor
+    Serial.print("frequencyID: ");
+    Serial.println(frequencyID);
+    Serial.print("pulseWidthID: ");
+    Serial.println(pulseWidthID);
+    Serial.println();
   }
 }
