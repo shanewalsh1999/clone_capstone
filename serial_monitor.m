847330537222cb2c58c99fe06e513a25b5525c40
shanewalsh1999 <shanewalsh1999@gmail.com>
2021-04-03 18:03:29 -0400

    update

--- serial_monitor.m
+++ serial_monitor.m
@@ -6,13 +6,12 @@ function serial_monitor
     instrreset;
     
     % s: serial monitor object of arduino
-    s = serial('/dev/cu.usbserial-1420');
+    s = serial('/dev/tty.usbserial-DN03GO3K');
     fopen(s);
-    
     
     %% Loop
     while 1
-        monitor = fscanf(s, '%i');
-        fprintf('%i\n', monitor);
+        monitor = fscanf(s, '%s');
+        fprintf('%s\n', monitor);
     end
 end
